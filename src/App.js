ac9c2edd1b6a51f4ed532b4c96a9614d9073cb85
Roozbeh Gholami <gholami.roozbeh70@gmail.com>
2022-02-17 21:04:27 +0330

    creatin menu component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComp';
 import './App.css';
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
      <Navbar dark color="primary">
       <div className='container'>
         <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
       </div>
      </Navbar>
+     <Menu />
     </div>
   );
 }
