9d409aad3d6911d55b363299c9a2f835eba8ae7d
Roozbeh Gholami <gholami.roozbeh70@gmail.com>
2022-02-18 00:22:06 +0330

    improving menu component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComp';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-
-function App() {
-  return (
-    <div>
-     <Navbar dark color="primary">
-      <div className='container'>
-        <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render () {
+    return (
+      <div>
+       <Navbar dark color="primary">
+        <div className='container'>
+          <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+        </div>
+       </Navbar>
+       <Menu dishes={this.state.dishes} />
       </div>
-     </Navbar>
-     <Menu />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
