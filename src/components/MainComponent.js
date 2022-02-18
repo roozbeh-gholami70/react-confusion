f9d0a77eebb312c64ca8fece13e3dcfb6d167ecf
Roozbeh Gholami <gholami.roozbeh70@gmail.com>
2022-02-19 00:06:04 +0330

    Added some css

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, Container } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from "./DishdetailComponent"
 import { DISHES } from '../shared/dishes';
@@ -22,10 +22,11 @@ class Main extends Component {
   render () {
     return (
       <div>
-       <Navbar dark color="primary">
-        <div className='container'>
-          <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-        </div>
+       <Navbar collapseOnSelect expand="lg" dark color="dark">
+       <Container>
+          <NavbarBrand href='/' className="mr-auto">Ristorante Con Fusion</NavbarBrand>
+
+        </Container>
        </Navbar>
        <Menu dishes={this.state.dishes} onClick={(dishId)=> this.onDishSelect(dishId)}/>
        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}  />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../style.css"
 
 
 
@@ -9,30 +10,32 @@ function RenderMenuItem(props){
     return(
         <Card onClick={()=>props.onClick(props.dish.id)} body
         color="secondary"
-        outline>
-            <CardImg width="100%" src={props.dish.image} alt={props.dish.name} />
-            <CardImgOverlay>
-                <CardTitle tag="h5">
+        outline >
+            <div className="hover-div_inner">
+                <CardImg width="100%" src={props.dish.image} alt={props.dish.name} />
+                    <CardImgOverlay>
+                    </CardImgOverlay>
+                <CardTitle tag="h5" className="m-2">
                     {props.dish.name}
                 </CardTitle>
-            </CardImgOverlay>
+            </div>
         </Card>
     );
 }
 const Menu = (props) => {
     const menu = props.dishes.map((dish) => {
         return (
-            <div key={dish.id} className="col-12 col-md-5 m-1">
+            <div key={dish.id} className="hover-div col-12 col-md-5 m-1">
                <RenderMenuItem dish={dish} onClick={props.onClick} />
             </div>
         );
     });
     return (
         <div className="container">
-
             <div className="row" >
                 {menu}
             </div>
+
         </div>
     );
 }
